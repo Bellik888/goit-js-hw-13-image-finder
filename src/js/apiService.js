30c18b133e4ff7f5439957bae33fdfb087b6c25f
Bellik888 <Bellik888@gmail.com>
2021-08-30 00:09:19 +0300

    add changes to getImages()

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-function getImages() {
-    axios.get('https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=dog&page=1&per_page=12&key=23143199');
+const BASE_URL = 'https://pixabay.com/api/'
+const KEY = '23143199-b6bad35488b5353448d934ebd'
+
+function getImages(value) {
+    let page = 1;
+    return axios.get(`${BASE_URL}?key=${KEY}&q=${value}&image_type=photo&orientation=horizontal&page=${page}&per_page=12`);    
 }
 
-export default getImages;
+export default getImages ;

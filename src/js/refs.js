d40423a2a568d0fbb4992a24d2fc91aa4743e0a8
Bellik888 <Bellik888@gmail.com>
2021-08-29 22:25:04 +0300

    add refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,7 @@
 const refs = {
     form: document.querySelector('#search-form'),
-    
+    searchBtn: document.querySelector('.search-btn'),
+    imgList: document.querySelector('img-list'),
 }
+
+export default refs;

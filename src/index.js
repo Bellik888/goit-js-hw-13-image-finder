765762ae85114b6bb34c80f10ea1b15b010ffe34
Bellik888 <Bellik888@gmail.com>
2021-08-30 00:07:51 +0300

    add cardTemplate

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-import refs from './js/refs';
+import refs from './js/refs.js';
+import getImages from './js/apiService.js';
+import CardTemplate from './templates/CardTemplate.hbs';
+
+refs.searchBtn.addEventListener('click', showImages);
+
+
+// let inputValue = refs.input.value
+
+function showImages(e) {
+    e.preventDefault();
+    
+    let inputValue = refs.input.value;
+    if (inputValue !== '') {
+        getImages(inputValue)
+            .then(response => cardUpdate(response.data.hits))
+            .catch(error => console.log(error));
+    }
+
+}
+
+function cardUpdate(response) {
+    const markup = CardTemplate(response);
+
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
